14cd9f9a7e35c43ce2272c7ba1fee4748971332e
clerge <cg@jmg-conseil.eu>
2022-03-02 14:03:06 +0100

    Add map and example

--- app.js
+++ app.js
@@ -5,17 +5,26 @@ const   express =   require('express'),
 
 app.use(express.static('public'));
 
+// Route pour la page d'accueil
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'front/index.html'));
 });
-
+// Route pour le jeu du shi-fu-mi
 app.get('/shifumi', function(req, res) {
     res.sendFile(path.join(__dirname, 'front/Shi-Fu-Mi/index.html'));
 });
-
+// Route pour le jeu du juste prix
 app.get('/justeprix', function(req, res) {
     res.sendFile(path.join(__dirname, 'front/Juste prix/index.html'));
 });
+// Route pour la carte OpenLayers
+app.get('/maps', function(req, res) {
+  res.sendFile(path.join(__dirname, 'front/Maps/index.html'));
+});
+// Route pour le CV de l'exercice du cours
+app.get('/example', function(req, res) {
+  res.sendFile(path.join(__dirname, 'front/example/index.html'));
+});
 
 app.listen(PORT);
-console.log('Server started at http://localhost:' + PORT);
+console.log('Le serveur tourne sur le port : ' + PORT);
