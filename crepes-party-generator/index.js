66fd9f7313ba5b45eb642f951a3f314e140647b1
ClG <94359348+ClrGe@users.noreply.github.com>
2022-03-04 11:33:13 +0100

    Update index.js

--- crepes-party-generator/index.js
+++ crepes-party-generator/index.js
@@ -1,39 +1,62 @@
-let button = document.getElementById("validationBtn");
-let nbInvites = document.getElementById("number-guest");
+ let button = document.getElementById("generate");
+            let farine  =   63,
+                oeuf    =   1,
+                lait    =   0.2,
+                sucre   =   0.5,
+                beurre  =   13;
 
-button.addEventListener("click", function () {
-    let newValue = parseInt(nbInvites.value);
-    nbInvites.value = newValue;
-});
-console.log(button, nbInvites.value);
+            function addCode() {
 
-const prompt    = require('prompt-sync')();
+                const   ing1Div = document.getElementById('ingredient01'),
+                        ing2Div = document.getElementById('ingredient02'),
+                        ing3Div = document.getElementById('ingredient03'),
+                        ing4Div = document.getElementById('ingredient04'),
+                        ing5Div = document.getElementById('ingredient05'),
+                        ing6Div = document.getElementById('ingredient06');
 
-let  flour      =   63;
-     eggs       =   1;
-     milk       =   0.2,
-     sugar      =   0.5,
-     butter     =   13,
-     numberOfGuests;
+                ing1Div.innerText = "Il faut " + farine + " g de farine";
+                ing2Div.innerText = "Il faut " + oeuf + " oeuf(s)";
+                ing3Div.innerText = "Il faut " + lait + " L de lait";
+                ing4Div.innerText = "Il faut " + sucre + " c.a.s de sucre";
+                ing5Div.innerText = "Il faut " + beurre + " g de beurre";
+                ing6Div.innerText = "";
 
-function ask() {
-    numberOfGuests = prompt("How many guests ?");
-    console.log("Number of guests : " + " " + numberOfGuests);
-    console.log("");
-        if(numberOfGuests >= 1){
-        flour   *=  numberOfGuests;
-        eggs    *=  numberOfGuests;
-        milk    *=  numberOfGuests;
-        sugar   *=  numberOfGuests;
-        butter  *=  numberOfGuests;
-        console.log("Butter: "  + Math.round(butter/100)*100 + "G");
-        console.log("Flour : "  + Math.round(flour/100)*100 + "G");
-        console.log("Eggs : "   + Math.round(eggs));
-        console.log("Milk : "   + Math.round(milk) + "L");
-        console.log("Sugar : "  + Math.round(sugar) + "G");
-    } else {
-        console.log("Please provide a valid number of guests");
-    }
-}
+                console.log("Vous faîtes des crêpes pour " + nbGuests + " personnes");
+                console.log("Il faut " + farine + " g de farine");
+                console.log("Il faut " + oeuf + " oeufs");
+                console.log("Il faut " + lait.toFixed(2) + " L de lait ");
+                console.log("Il faut " + sucre + " g de sucre");
+                console.log("Il faut " + beurre + " g de beurre");
+                    
+                return "Next";
+            }
+            button.addEventListener("click", function (e) {
+                let nbGuests = document.getElementById("nbGuests").value;
+                if (nbGuests <= 0) {
+                    alert("Il faut renseigner un nombre valide")
+                } else {
+                    farine  =   63,
+                    oeuf    =   1,
+                    lait    =   0.2,
+                    sucre   =   0.5,
+                    beurre  =   13;
+                    let newValue = parseInt(nbGuests.value);
+                    nbGuests.value = newValue;
 
-ask();
+                    farine  *=  nbGuests;
+                    oeuf    *=  nbGuests;
+                    lait    *=  nbGuests;
+                    sucre   *=  nbGuests;
+                    beurre  *=  nbGuests;
+
+                    addCode();
+
+                    return "Bon appétit";
+                }
+            });
+            console.log(button, nbGuests.value);
+
+            let  lotOeuf    =   12,
+                 achatOeuf  =   Math.ceil(oeuf / lotOeuf);
+                 
+            console.log(achatOeuf + " pack de " + lotOeuf + " oeufs");
